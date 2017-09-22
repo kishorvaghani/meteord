6aa3e21325b0f20739e99d627c4dabeaaa168157
kishorvaghani <kishor.upmetrics@gmail.com>
2017-09-22 18:58:40 +0530

    wget in wkhtmltopdf

--- base/scripts/lib/install_wkhtmltopdf.sh
+++ base/scripts/lib/install_wkhtmltopdf.sh
@@ -2,6 +2,9 @@
 # Install Wkhtmltopdf
 set -e
 
+apt-get update
+apt-get install -y wget chrpath libssl-dev libxft-dev
+
 WKHTMLTOPDF_VERSION=0.12.4
 WKHTMLTOPDF_TAR_FILE=wkhtmltox-${WKHTMLTOPDF_VERSION}_linux-generic-amd64.tar.xz
 
@@ -11,3 +14,4 @@ tar xvf $WKHTMLTOPDF_TAR_FILE
 mv wkhtmltox/bin/wkhtmlto* /usr/bin/
 
 rm $WKHTMLTOPDF_TAR_FILE
+apt-get -y purge wget
